7697dec8b2bcbae3b51c0147041e011da2be3592
Maxim Gumeniuk <108987194+Maxim-Gumeniuk@users.noreply.github.com>
2023-11-24 11:32:42 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import clsx from "clsx";
+
+export default function Home() {
+    return (
+        <h1 className="flex flex-column justify-center items-center h-screen">
+            hello my pet App
+        </h1>
+    );
+}
---
+++ src/app/registration/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+    return (
+        <div>
+            <div>
+                <div>registration</div>
+                <div>login</div>
+            </div>
+            <div>
+                <input />
+            </div>
+        </div>
+    );
+}
